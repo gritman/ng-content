750f21c6877ef42c18cf3b48167681066bf5703e
gritman <gritman@126.com>
2017-05-30 20:22:57 +0800

    contentInit和contentCheck顺序

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterContentChecked, AfterContentInit, AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit, AfterContentChecked, AfterViewInit{
+
   title = 'app works!';
   divContent = '<div>父组件属性</div>';
+
+  ngAfterContentInit(): void {
+    console.log('父组件投影内容初始化完毕');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('父组件投影内容检测完毕');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('父组件视图内容初始化完毕');
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentChecked, AfterContentInit, AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements AfterContentInit, AfterContentChecked, AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterContentInit(): void {
+    console.log('子组件投影内容初始化完毕');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('子组件投影内容检测完毕');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('子组件视图内容初始化完毕');
   }
 
 }
